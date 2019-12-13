a024a6e443ffb32ddda9844ac9be2d628673933f
Ajschlander <Aschlander21@gmail.com>
2019-12-12 20:51:38 -0600

    CLI now will continue asking for more engineer's and intern's until the user stops

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const fs          = require("fs"),
       Intern      = require("./lib/intern.js"),
       team        = [];
 
-
-
-
 const addManager = () => {
     inquirer
         .prompt([
@@ -30,22 +27,132 @@ const addManager = () => {
                 type: "number",
                 message: "What is your office number?",
                 name: "officeNumber"
-            },
+            }
         ])
         .then(answers => {
+            const managerName           = answers.name,
+                  managerID             = answers.id,
+                  managerEmail          = answers.email,
+                  managerOfficeNumber   = answers.officeNumber;
 
+            const manager = new Manager(managerName, managerID, managerEmail, managerOfficeNumber);
+            team.push(manager);
+            addTeamMember();
         });
 }
 
+const addEngineer = () => {
+    inquirer
+        .prompt([{
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "name"
+            },
+            {
+                type: "number",
+                message: "What is the engineer's id number?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's github name?",
+                name: "githubName"
+            }
+        ])
+        .then(answers => {
+            const engineerName        = answers.name,
+                  engineerID          = answers.id,
+                  engineerEmail       = answers.email,
+                  engineerGithubName  = answers.githubName;
 
-function addTeamMember() {
+            const engineer = new Engineer(engineerName, engineerID, engineerEmail, engineerGithubName);
+            team.push(engineer);
+            confirmAddMember();
+        });
+}
+
+const addIntern = () => {
+    inquirer
+        .prompt([{
+                type: "input",
+                message: "What is the intern's name?",
+                name: "name"
+            },
+            {
+                type: "number",
+                message: "What is the intern's id number?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the intern's email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What school is the intern attending?",
+                name: "school"
+            }
+        ])
+        .then(answers => {
+            const internName      = answers.name,
+                  internID        = answers.id,
+                  internEmail     = answers.email,
+                  internSchool    = answers.school;
+            
+            const intern = new Intern(internName, internID, internEmail, internSchool);
+            team.push(intern);
+            confirmAddMember();
+        });
+}
+
+const confirmAddMember = () => {
     inquirer
         .prompt([
-            
+            {
+                type: "confirm",
+                message: "Would you like to add another team member?",
+                name: "confirmAdd"
+            }
         ])
-        .then(answers => {
-            
+        .then(answer => {
+            if(answer.confirmAdd === true){
+                addTeamMember();
+            }
+            else{
+                // done
+                console.log(team);
+            }
         });
 }
 
-addTeamMember();
+const addTeamMember = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern"
+                ],
+                name: "memberType"
+            }    
+        ])
+        .then(answers => {
+            const newMemberType = answers.memberType;
+            if(newMemberType === "Engineer"){
+                addEngineer();
+            }
+            else{
+                addIntern();
+            }
+        });
+}
+
+addManager();
