5ae0ceeb9890de5cf9a34035c07e83eea5127a49
Ajschlander <Aschlander21@gmail.com>
2019-12-12 20:01:44 -0600

    Completed testing and started inquirer prompts

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
+const fs          = require("fs"),
+      inquirer    = require("inquirer"),
+      Manager     = require("./lib/manager.js"),
+      Engineer    = require("./lib/engineer.js"),
+      Intern      = require("./lib/intern.js"),
+      team        = [];
 
 
-function getUserInput() {
+
+
+const addManager = () => {
     inquirer
         .prompt([
             {
+               type: "input",
+               message: "What is your name?",
+               name: "name" 
+            },
+            {
+                type: "number",
+                message: "What is your id number?",
+                name: "id"
+            },
+            {
                 type: "input",
-                message: "Enter a GitHub username:",
-                name: "username"
-            }
+                message: "What is your email?",
+                name: "email"
+            },
+            {
+                type: "number",
+                message: "What is your office number?",
+                name: "officeNumber"
+            },
+        ])
+        .then(answers => {
+
+        });
+}
+
+
+function addTeamMember() {
+    inquirer
+        .prompt([
+            
         ])
         .then(answers => {
             
         });
 }
+
+addTeamMember();
